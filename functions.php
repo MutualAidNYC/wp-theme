85e6153d95b62a5f8fdad4daffe04bd4387c3d76
Echo G <ChaosExAnima@users.noreply.github.com>
2020-05-13 19:53:12 -0400

    Adds prelim block SSR approach.

--- functions.php
+++ functions.php
@@ -157,6 +157,18 @@ if ( function_exists( 'register_block_style' ) ) {
 }
 
 /**
+ * Registers block types.
+ */
+if ( function_exists( 'register_block_type' ) ) {
+	register_block_type(
+		'mutualaidnyc/resources',
+		array(
+			'render_callback' => __NAMESPACE__ . '\\render_block_resources',
+		)
+	);
+}
+
+/**
  * Add Google webfonts
  *
  * @return string
@@ -209,4 +221,41 @@ function register_customizer( WP_Customize_Manager $wp_customize ) : void {
 	foreach ( $controls as $control ) {
 		$wp_customize->remove_control( $control );
 	}
+}
+
+/**
+ * Render callback for Resources Block.
+ *
+ * @since 1.0.1
+ * @return void
+ */
+function render_block_resources() : void {
+	if ( ! class_exists( 'AirpressQuery' ) ) {
+		return;
+	}
+
+	$needs_query = new \AirpressQuery( 'Ref - Need', 0 );
+	$needs_query->addFilter( 'NOT({Resources} = BLANK())' );
+	$needs_query->addFilter( 'NOT({Need} = "-Not Listed")' );
+	$needs_query->sort( 'Need' );
+
+	$resources_query = new \AirpressQuery( 'Resources', 0 );
+	$resources_query->addFilter( '{Publish Status of Resource} = "Published"' );
+
+	$needs = new \AirpressCollection( $needs_query );
+	$needs->populateRelatedField( 'Resources', $resources_query );
+
+	echo '<ul>';
+	foreach ( $needs as $need ) {
+		printf( '<li>%s <ul>', esc_html( $need['Need'] ) );
+		foreach ( $need['Resources'] as $resource ) {
+			printf(
+				'<li><a href="%2$s">%1$s</a></li>',
+				esc_html( $resource['Resource Title'] ),
+				esc_url( $resource['Link to Resource'] ?? '' ),
+			);
+		}
+		echo '</ul></li>';
+	}
+	echo '</ul>';
 }
