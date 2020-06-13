3965eb5ce18ba4529387dcad93ed64b477518702
Echo G <ChaosExAnima@users.noreply.github.com>
2020-06-13 16:38:04 -0400

    Adds sidebar template.

---
+++ templates/template-sidebar.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Template Name: Sidebar Template
+ * Template Post Type: page
+ *
+ * @package WordPress
+ * @subpackage MutualAidNYC
+ * @since 1.0.4
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+	if ( have_posts() ) :
+		the_post();
+		?>
+		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<?php get_template_part( 'template-parts/header-cover' ); ?>
+
+			<div class="post-inner" id="post-inner">
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+
+					<?php edit_post_link(); ?>
+				</div>
+
+				<div class="page-sidebar">
+					<?php dynamic_sidebar( 'page-sidebar' ); ?>
+				</div>
+			</div>
+		</article><!-- .post -->
+		<?php
+	endif;
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
