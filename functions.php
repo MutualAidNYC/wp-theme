35a1e55f76b5a0bd4606868da3b591526f52da66
Echo G <ChaosExAnima@users.noreply.github.com>
2020-06-13 16:38:49 -0400

    Sets default template to cover.

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@ function setup() : void {
 	add_filter( 'get_custom_logo', __NAMESPACE__ . '\\filter_logo' );
 	add_action( 'customize_controls_enqueue_scripts', __NAMESPACE__ . '\\remove_customizer_scripts', 11 );
 	add_action( 'customize_register', __NAMESPACE__ . '\\register_customizer', 11 );
+	add_filter( 'body_class', __NAMESPACE__ . '\\filter_body_class' );
 
 	add_filter( 'trp_skip_gettext_processing', __NAMESPACE__ . '\\skip_jetpack_translation', 10, 4 );
 
@@ -235,6 +236,22 @@ function filter_logo( string $html ) : string {
 	// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 	$svg_markup = file_get_contents( __DIR__ . '/assets/logo.svg' );
 	return str_replace( '></', ">$svg_markup</", $html );
+}
+
+/**
+ * Adds overlay header to pages with no template.
+ *
+ * @param array $classes Array of classes on the body tag.
+ * @return array
+ */
+function filter_body_class( array $classes ) : array {
+	if (
+		is_page() && 
+		( ! is_page_template() || is_page_template( 'templates/template-sidebar.php' ) )
+	) {
+		$classes[] = 'overlay-header';
+	}
+	return $classes;
 }
 
 /**
---
+++ page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Default page template
+ *
+ * @package WordPress
+ * @subpackage MutualAidNYC
+ * @since 1.0.4
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+	if ( have_posts() ) :
+		the_post();
+		?>
+		<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+			<?php get_template_part( 'template-parts/header-cover.php' ); ?>
+
+			<div class="post-inner" id="post-inner">
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+
+				<?php edit_post_link(); ?>
+			</div>
+		</article><!-- .post -->
+		<?php
+	endif;
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
