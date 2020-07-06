27204875b67387da580903a033776b81d8ce696f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-07-06 19:44:15 -0400

    Fixes issue with close milestone script.

--- .github/close-milestone.sh
+++ .github/close-milestone.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-source .github/milestone-utils.sh
-
-get_milestone()
-
 curl --request PATCH \
   --url https://api.github.com/repos/${GITHUB_REPOSITORY}/milestones/${MILESTONE_NUMBER} \
   --header "Authorization: Bearer ${GITHUB_TOKEN}" \
