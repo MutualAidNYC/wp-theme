04c79f8d1244527f871ac2ab2dd6afe13f0e10b4
Echo <ChaosExAnima@users.noreply.github.com>
2020-07-06 20:42:16 -0400

    Automatically assigns the latest milestone (#88)
    * Fixes issue with close milestone script.
    
    * Adds assign milestone script.
    
    * Updates workflow.
    
    * Adds test for current branch.
    
    * Test.
    
    * Updates
    
    * Tries a different way to get the vars.
    
    * Tries a different approach.
    
    * Actually checkouts out files.
    
    * Makes things writable and tries again.
    
    * Right, a quote.
    
    * Tries to use GitHub Script.
    
    * Logging!
    
    * Okay new try!
    
    * logging?
    
    * wait what
    
    * okay the entire context
    
    * Removes logging.
    
    * Adds reopening to list.
    
    * Removes test push.
    
    * Removes extra scripts.

--- .github/get-milestone.sh
+++ .github/get-milestone.sh
@@ -6,3 +6,5 @@ MILESTONE_NAME=`echo "$JSON" | grep '"title":' | sed -E 's/.*: "([^"]+).*/\1/'`
 
 echo "::set-env name=MILESTONE_NUMBER::${MILESTONE_NUMBER}"
 echo "::set-env name=MILESTONE_NAME::${MILESTONE_NAME}"
+
+echo "Set environment vars: ${MILESTONE_NUMBER} and ${MILESTONE_NAME}"
