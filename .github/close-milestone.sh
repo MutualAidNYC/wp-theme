8c16a8acf63f1f87d5c1607fbcf6621b267fd529
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2020-07-06 20:38:55 -0400

    Removes extra scripts.

--- .github/close-milestone.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl --request PATCH \
-  --url https://api.github.com/repos/${GITHUB_REPOSITORY}/milestones/${MILESTONE_NUMBER} \
-  --header "Authorization: Bearer ${GITHUB_TOKEN}" \
-  --header 'Content-Type: application/json' \
-  --data '{"state":"closed"}'
