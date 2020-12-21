744bb992020e88f21f57d8f663469d38aaa759a2
Joshua Kwan <joshua.m.kwan@gmail.com>
2020-12-20 21:02:10 -0500

    typo

--- .github/get-milestone.sh
+++ .github/get-milestone.sh
@@ -6,7 +6,7 @@ MILESTONE_NAME=`echo "$JSON" | grep '"title":' | sed -E 's/.*: "([^"]+).*/\1/'`
 
 cat >"${GITHUB_ENV}" <<EOF
 MILESTONE_NUMBER=${MILESTONE_NUMBER}
-MILESTONE_NAME::${MILESTONE_NAME}
+MILESTONE_NAME=${MILESTONE_NAME}
 EOF
 
 echo "Set environment vars: ${MILESTONE_NUMBER} and ${MILESTONE_NAME}"
